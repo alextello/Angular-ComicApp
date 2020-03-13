86b766607405320335b0fb944cfbf71f9c5d7f60
Edwin Alejandro Tello Santizo <alextello1@hotmail.com>
2020-03-13 10:14:36 -0600

    Añadido buscador de héroes

---
+++ src/app/components/buscar-heroe/buscar-heroe.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-buscar-heroe',
+  templateUrl: './buscar-heroe.component.html',
+  styleUrls: ['./buscar-heroe.component.css']
+})
+export class BuscarHeroeComponent implements OnInit {
+termino: string;
+heroes: Heroe[];
+  constructor(private activatedRoute: ActivatedRoute,
+              private _heroesService: HeroesService,
+              private  _router: Router) {
+
+     }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.termino = params.termino;
+      this.heroes = this._heroesService.buscarHeroes(this.termino);
+    });
+  }
+
+  verHeroe(idx: number){
+    this._router.navigate(['/heroe', idx]);
+  }
+
+}
