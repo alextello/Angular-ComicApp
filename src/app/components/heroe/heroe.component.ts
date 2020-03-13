ea0e731775b78299f5c64a97739a740335ffbae6
Edwin Alejandro Tello Santizo <alextello1@hotmail.com>
2020-03-12 20:57:47 -0600

    Despliegue de informacion de heroe individual

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: []
+  styles: ['imagen { max-height: 150px; max-width: 150px; }']
 })
 export class HeroeComponent implements OnInit {
-
-  constructor() { }
-
+  heroe: any = {};
+  colorCasa = 'danger';
+  constructor(private activatedRoute: ActivatedRoute,
+              private _heroesService: HeroesService) { 
+                this.activatedRoute.params.subscribe(params => {
+                this.heroe = this._heroesService.getHeroe(params.id);
+                if(this.heroe.casa === 'DC') {
+                  this.colorCasa = 'primary';
+                }
+              });
+  }
   ngOnInit(): void {
   }
 
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -20,7 +20,7 @@ export class HeroesComponent implements OnInit {
   }
 
   verHeroe(idx: number){
-    this._router.navigate(['/heroe', idx])
+    this._router.navigate(['/heroe', idx]);
   }
 
 }
